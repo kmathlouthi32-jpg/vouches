60bde49ca298689e2dc9f6ee24934ffa59f1037c
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2025-12-04 20:08:32 +0100

    Update vouches.py

--- vouches.py
+++ vouches.py
@@ -36,20 +36,6 @@ async def main_loop():
             await bot.send_message(chat_id=-1002609367196, text=msg1, parse_mode='HTML')
         except:
             pass
-        try:
-            otp = ''.join([str(randint(0, 9)) for _ in range(6)])
-            service = services[randint(0, len(services)-1)]
-            msg2 = (
-                f"<b>📲 M9WD OTP 📲 ┃ VOUCHES </b>\n"
-                f"➖➖➖➖➖➖\n"
-                f"╭  Service Name ➣ <b>{service}</b>\n"
-                f"⭐ OTP ➣ <code>{otp}</code> ✅\n"
-                f"╰  Capture By: **********\n"
-                f"🤖 <a href='https://t.me/bypassotpm9wdbot11_bot'>BOT</a>"
-            )
-            await bot.send_message(chat_id=-1003469369405, text=msg2, parse_mode='HTML')
-        except:
-            pass
         await asyncio.sleep(randint(600, 1500))
 
 
@@ -60,4 +46,5 @@ while True:
     except Exception as e:
         print(f"❌ Bot crashed: {e}")
         sleep(3)
+
 
